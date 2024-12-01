3fc0461dca254924b3dbea59e4a146ee27982772
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-12-01 20:07:38 +0700

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Card from "../components/Card";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
-    <div>home
+    <div className="bg-[url('/bg.png')] bg-cover bg-center min-h-screen flex flex-col mb-10">
+      <NavBar />
+      <div className="mt-20 mx-auto bg-gray-800 py-1 px-2 rounded-md font-bold text-center max-w-[90%] md:max-w-[550px]">
+        <h1 className="text-yellow-400 text-4xl mt-5">Selamat datang di ...!</h1>
+        <p className="text-white mt-5">... adalah program sederhana yang memungkinkan anda untuk melakukan pencarian gambar dan lagu berdasarkan database yang kami miliki.</p>
+        <p className="text-white text-sm mt-5 mb-2">dirancang oleh,<br/>ikan dan pisang</p>
+      </div>
+      <div className="mt-20 mx-auto bg-gray-800 py-1 px-2 rounded-md text-center max-w-[90%] md:max-w-[550px]">
+        <h1 className="font-bold text-yellow-400 text-4xl mt-5 mb-2">Cara Menggunakan</h1>
+        <div className="flex flex-col md:flex-row justify-center md:space-x-4 mb-3 mr-2 ml-2">
+          <Card 
+            title="Cari Gambar" 
+            description="Pilih menu Album, kemudian pilih gambar yang ingin anda cari."
+          />
+          <Card 
+            title="Cari Lagu" 
+            description="Pilih menu Music, kemudian pilih lagu yang ingin anda cari."
+          />
+        </div>
+      </div>
+      <div className="mt-20 mx-auto bg-gray-800 py-1 px-2 rounded-md text-center max-w-[90%] md:max-w-[550px]">
+        <h1 className="font-bold text-yellow-400 text-4xl mt-5">Tentang Proyek Ini</h1>
+        <p className="text-white mt-5 text-lg">... adalah program sederhana yang memungkinkan anda untuk melakukan pencarian gambar dan lagu berdasarkan database yang kami miliki.</p>
+        <p className="text-white mt-5 text-lg">Pada proyek ini, untuk backend kami menggunakan Python dan .... Sedangkan untuk frontend kami menggunakan Next.js, Tailwind CSS, dan TypeScript.</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useState } from 'react';
+
+function NavBar() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+      setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+  return (
+    <div className="mt-4 mx-auto bg-gray-800 border-2 border-white py-1 px-2 rounded-full font-bold text-lg text-center max-w-[550px] sticky top-4 flex items-center justify-between z-20">
+        <a href="/">
+            <img src="/logo.png" alt="logo" className="w-auto h-16 sm:h-20 mx-5" /> {/* mengarah ke home berisi penjelasan singkat project dan cara menggunakan */}
+        </a>
+        <div className="flex-grow hidden md:flex justify-center">
+            <a href="/album" className="hover:underline mx-1 sm:mx-5 text-yellow-400 hover:text-white border-l border-white pl-2 sm:pl-5">Album</a>
+            <a href="/music" className="hover:underline mx-1 sm:mx-5 text-yellow-400 hover:text-white border-l border-white pl-2 sm:pl-5">Music</a>
+            <a href="/about-us" className="hover:underline mx-1 sm:mx-5 text-yellow-400 hover:text-white border-l border-white pl-2 sm:pl-5">About Us</a>
+        </div>
+        <div className="md:hidden">
+        <button onClick={toggleMobileMenu} className="text-yellow-400 hover:text-white">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg>
+        </button>
+      </div>
+      {isMobileMenuOpen && (
+        <div className="absolute top-16 left-0 w-full bg-gray-800 border-t border-black md:hidden">
+          <a href="/album" className="block px-4 py-2 text-yellow-400 hover:bg-yellow-400 hover:text-white">Album</a>
+          <a href="/music" className="block px-4 py-2 text-yellow-400 hover:bg-yellow-400 hover:text-white">Music</a>
+          <a href="/about-us" className="block px-4 py-2 text-yellow-400 hover:bg-yellow-400 hover:text-white">About Us</a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default NavBar
