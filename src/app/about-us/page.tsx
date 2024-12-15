2cc3cf8a04930baefb6506b852df992dafd717a1
Bertha Soliany Frandi <163696570+BerthaSoliany@users.noreply.github.com>
2024-12-16 06:16:44 +0700

    style: fix home and about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -4,25 +4,25 @@ import React from 'react'
 
 function AboutUs() {
   return (
-    <div className="bg-[url('/image1.png')] bg-cover bg-center min-h-screen flex flex-col p-2 md:p-4 font-custom">
+    <div className="bg-[url('/image1.png')] bg-cover bg-center min-h-screen flex flex-col p-2 md:p-4 font-custom space-y-10 items-center">
       <NavBar />
       <p className="font-bold text-white text-5xl text-center mt-6">About Us</p>
       <Profile 
-        image1='/ash.png'
+        image1='/eiji.png'
         image2="/bertha.jpg"
         name="Bertha Soliany Frandi"
         nim="13523026"
         quote="mau turu tapi belum kelar :<"
       />
       <Profile 
-        image1='/eiji.png'
+        image1='/drug.png'
         image2="/max.jpg"
         name="Rafen Max Alessandro"
         nim="13523031"
         quote="..."
       />
       <Profile 
-        image1='/wong.png'
+        image1='/ash.png'
         image2="/grace.jpg"
         name="Grace Evelyn Simon"
         nim="13523087"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,33 @@ export default function Home() {
         <div className="bg-black bg-opacity-50 py-10 px-5 rounded-3xl font-bold text-center max-w-[90%] md:max-w-[1000px] space-y-10">
           <h1 className="text-white text-3xl">Selamat datang di <br/>Ikan dan Pisang!</h1>
           <p className="text-white text-md">Ikan dan Pisang adalah program sederhana yang memungkinkan untuk melakukan pencarian gambar album dan lagu berdasarkan database.</p>
-          <p className="text-white text-sm">dirancang oleh,<br/>ikan dan pisang</p>
+          <p className="text-white text-sm">dibuat oleh<br/> tim ikan dan pisang</p>
         </div>
         <div className="bg-black bg-opacity-50 py-10 px-5 rounded-3xl text-center max-w-[90%] md:max-w-[1000px] space-y-5">
           <h1 className="font-bold text-white text-4xl">Cara Menggunakan</h1>
           <div className="flex flex-col md:flex-row justify-center md:space-x-4">
             <Card 
               title="Cari Album" 
-              description="Pergi ke menu Finder, Upload album yang ingin dicari, Upload dataset yang ingin digunakan, Klik tombol 'Album', daftar lagu yang berhubungan dengan album dapat dilihat dengan klik album yang bersangkutan"
+              description1="1. Pergi ke menu Finder"
+              description2="2. Upload album yang ingin dicari"
+              description3="3. Upload dataset yang ingin digunakan"
+              description4="4. Klik tombol 'Album'"
+              description5="daftar lagu yang berhubungan dapat dilihat dengan klik album yang bersangkutan"
             />
             <Card 
               title="Cari Lagu" 
-              description="Pergi ke menu Finder, Upload lagu yang ingin dicari, Upload dataset yang ingin digunakan, Klik tombol 'lagu'"
+              description1="1. Pergi ke menu Finder"
+              description2="2. Upload lagu yang ingin dicari"
+              description3="3. Upload dataset yang ingin digunakan"
+              description4="4. Klik tombol 'lagu'"
             />
           </div>
         </div>
-        <div className="bg-black bg-opacity-50 py-10 px-5 rounded-3xl text-center max-w-[90%] md:max-w-[1000px] space-y-5">
+        <div className="bg-black bg-opacity-50 py-10 px-5 rounded-3xl text-center max-w-[90%] md:max-w-[1000px] space-y-10">
           <h1 className="font-bold text-white text-4xl">Tentang Proyek Ini</h1>
-          <p className="text-white text-sm">Ikan dan Pisang adalah program sederhana yang memungkinkan anda untuk melakukan pencarian gambar album dan lagu berdasarkan.</p>
+          <p className="text-white text-sm">Ikan dan Pisang adalah program sederhana yang memungkinkan anda untuk melakukan pencarian gambar album dan lagu berdasarkan dataset.</p>
+          <p className="text-white text-sm">Terdapat dua fungsi besar yang membuat pencarian album dan lagu dapat dilakukan, yaitu Music Information Retrival (MIR) dan Album Information Retrival (AIR).
+          Terdapat pula mapper yang menghubungkan album dan lagu sehingga pencarian album dapat menampilkan lagu dengan album yang sama dan pencarian lagu mempunyai gambar album yang sesuai.</p>
           <p className="text-white text-sm">Pada proyek ini, untuk backend kami menggunakan Python dan Flask. Sedangkan untuk frontend kami menggunakan Next.js, Tailwind CSS, dan TypeScript.</p>
         </div>
       </div>
